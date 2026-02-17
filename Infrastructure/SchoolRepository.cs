7fadf964e4f0128872d8b18cae4fd124c868c2ca
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-02-17 18:10:16 +0100

    Mody Repository

--- Infrastructure/SchoolRepository.cs
+++ Infrastructure/SchoolRepository.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolApp.Core.Entities;
 using SchoolApp.Core.Interfaces;
 
 namespace SchoolApp.Infrastructure;
@@ -5,6 +7,45 @@ namespace SchoolApp.Infrastructure;
 public class SchoolRepository(SchoolDbContext db) : IRepository
 {
     private readonly SchoolDbContext _db = db;
-    public void Add()
-    { }
+
+    public void Save()
+    {
+        _db.SaveChanges();
+    }
+
+    public Course? GetCourseById(int id)
+    {
+        return _db.Courses
+        .Include(c => c.Students)
+        .FirstOrDefault(c => c.CourseId == id);
+    }
+
+    public Student? GetStudentById(int id)
+    {
+        return _db.Students
+            .Include(s => s.Courses)
+            .FirstOrDefault(s => s.StudentId == id);
+    }
+
+    public List<Student> GetAllStudent()
+    {
+        return _db.Students.ToList();
+    }
+
+    public List<Course> GetAllCourses()
+    {
+        return _db.Courses.ToList();
+    }
+
+    public void AddStudent(Student student)
+    {
+        _db.Add(student);
+        _db.SaveChanges();
+    }
+
+    public void AddCourse(Course course)
+    {
+        _db.Add(course);
+        _db.SaveChanges();
+    }
 }
