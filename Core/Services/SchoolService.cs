af04faffcac3728e96e94c5dec454f909bc0c251
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-02-17 18:10:57 +0100

    Modify SchoolService

--- Core/Services/SchoolService.cs
+++ Core/Services/SchoolService.cs
@@ -9,13 +9,62 @@ public class SchoolService(IRepository repo)
     private readonly IRepository _repo = repo;
 
     public List<Student> GetAllStudents()
-    { }
+    {
+        return _repo.GetAllStudent();
+    }
     public List<Course> GetAllCourses()
-    { }
-    public void AddStudent(Student student)
-    { }
-    public void AddCourse(Course course)
-    { }
-    public void EnrollStudent(int studentId, int courseId)
-    { }
+    {
+        return _repo.GetAllCourses();
+    }
+    public void AddStudent(string? name)
+    {
+        var newStudent = new Student
+        { Name = name };
+
+        _repo.AddStudent(newStudent);
+    }
+    public void AddCourse(string? title)
+    {
+        var course = new Course { Title = title };
+
+        _repo.AddCourse(course);
+    }
+    public void HandleEnrollment(int studentId, int courseId)
+    {
+        var student = _repo.GetStudentById(studentId);
+        var course = _repo.GetCourseById(courseId);
+
+        bool success = HandleEnrollmentErrors(student, course);
+
+        if (!success)
+        {
+            return;
+        }
+
+        course!.Students.Add(student!);
+        _repo.Save();
+    }
+
+    static bool HandleEnrollmentErrors(Student? student, Course? course)
+    {
+        bool success = true;
+        Console.ForegroundColor = ConsoleColor.Red;
+        if (student == null || course == null)
+        {
+            Console.WriteLine("Student eller kurs hittades inte.");
+            success = false;
+        }
+        else if (course.Students.Contains(student))
+        {
+            Console.WriteLine("Studenten är redan inskriven i kursen.");
+            success = false;
+        }
+        else if (course.Students.Count >= 30)
+        {
+            Console.WriteLine("Kursen är full.");
+            success = false;
+        }
+        Console.ResetColor();
+        return success;
+    }
 }
