068094ef32e7e0c7b6ea3cd0f2c1d7b6854fbb90
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-02-17 18:09:37 +0100

    Modify IRepository

--- Core/Interfaces/lRepository.cs
+++ Core/Interfaces/lRepository.cs
@@ -1,6 +1,14 @@
+using SchoolApp.Core.Entities;
+
 namespace SchoolApp.Core.Interfaces;
 
 public interface IRepository
 {
-    public void Add();
+    public void Save();
+    public Course? GetCourseById(int id);
+    public Student? GetStudentById(int id);
+    public List<Student> GetAllStudent();
+    public List<Course> GetAllCourses();
+    public void AddStudent(Student student);
+    public void AddCourse(Course course);
 }
